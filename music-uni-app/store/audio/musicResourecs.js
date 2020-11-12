a0abfb49f4a3fafd25c13f11e9c2087cd36ac288
CodeJasmine <2816540069@qq.com>
2020-11-12 13:26:24 +0800

    音乐播放器组件

---
+++ music-uni-app/store/audio/musicResourecs.js
@@ -0,0 +1,27 @@
+export default{
+	musicResourecs: [{
+		id: 20001,
+		name: "不要说话",
+		src: '/static/mp3/bysh.mp3',
+		singer: {
+			name: "陈奕迅",
+			synopsis: "陈奕迅（Eason Chan），1974年7月27日出生于中国香港，祖籍广东省东莞市，华语流行乐男歌手、演员，毕业于英国金斯顿大学。"
+		}
+	},{
+		id: 20002,
+		name: "偏爱",
+		src: '/static/mp3/pa.mp3',
+		singer: {
+			name: "张芸京",
+			synopsis: "张芸京（Jing chang），1983年9月6日出生于台湾省台北市，中国台湾流行乐女歌手，毕业于台北市立松山高级商业家事职业学校广告设计科。"
+		}
+	},{
+		id: 20003,
+		name: "无人之岛",
+		src: '/static/mp3/wrzd.mp3',
+		singer: {
+			name: "任然",
+			synopsis: "任然，1989年7月3日出生于四川省成都市，中国内地流行乐女歌手。"
+		}
+	}]
+}
