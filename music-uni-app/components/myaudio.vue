f76ce3b9aca42f4859aea213863693e646d866fc
CodeJasmine <2816540069@qq.com>
2020-11-12 17:46:07 +0800

    歌曲详情页、夜间模式

--- music-uni-app/components/myaudio.vue
+++ music-uni-app/components/myaudio.vue
@@ -1,29 +1,30 @@
 <template>
-	<view class="fixed-bottom rounded mx-2 mb-1" style="height: 160rpx;background-color: #D6D5B7;opacity: .9;">
+	<view class="fixed-bottom rounded mx-2 mb-1" style="height: 160rpx;background-color: #d1ccc0;opacity: .9;" @tap="toDetailPage">
 		<!-- 进度部分 -->
-		<view class="flex align-center justify-center font" style="color: #7a8388;height: 65rpx;">
+		<view class="flex align-center justify-center font" style="color: #7a8388;height: 65rpx;" @tap.stop>
 			<!-- 总时长 -->
-			<view>{{ durationTime | formatTime }}</view>
+			<view>{{durationTime|formatTime}}</view>
 			<!-- 进度条部分 -->
-			<view style="width: 500rpx ;display: inline;">
-				<slider @change="sliderToPlay" @changing="sliderToPlay" block-size="16" activeColor="#e48267" backgroundColor="#eef2f3"
-				 :max="durationTime" :value="currentTime" style="display: inline-block;width: 350rpx;" />
-				<!-- 播放 -->
-				<view style="display: inline-block;">{{ currentTime | formatTime }}</view>
+			<view style="width: 500rpx;">
+				<slider block-size="16" activeColor="#e48267" backgroundColor="#eef2f3" :max="durationTime" :value="currentTime"
+				 @change="sliderToPlay" @changing="sliderToPlay" />
 			</view>
+			<!-- 播放时刻 -->
+			<view>{{currentTime|formatTime}}</view>
 		</view>
+
 		<view class="flex align-center justify-between mx-2" style="height: 95rpx;">
 			<!-- 音频相关信息 -->
 			<view class="font" style="color: #424651;">
-				<view>歌曲-{{ audioName }}</view>
-				<view>歌手-{{ singerName }}</view>
+				<view>歌手-{{singerName}}</view>
+				<view>歌曲-{{audioName}}</view>
 			</view>
 			<!-- 音频按钮部分 -->
-			<view class="flex align-center">
+			<view class="flex align-center" @tap.stop>
 				<view class="animated" hover-class="pulse">
 					<my-icon iconId="icon-shangyishou" iconSize="75" @my-click="PreOrNext('pre')"></my-icon>
 				</view>
-				<my-icon :iconId="playStatus ? 'icon-bofang' : 'icon-ziyuan'" iconSize="75" class="mx-2" @my-click="PlayOrPause"></my-icon>
+				<my-icon :iconId="playStatus?'icon-bofang':'icon-ziyuan'" iconSize="75" class="mx-2" @my-click="PlayOrPause"></my-icon>
 				<view class="animated" hover-class="pulse">
 					<my-icon iconId="icon-xiayishou" iconSize="75" @my-click="PreOrNext('next')"></my-icon>
 				</view>
@@ -38,8 +39,8 @@
 		mapGetters,
 		mapMutations,
 		mapActions
-	} from 'vuex'
-	import unit from '../common/unit.js'
+	} from 'vuex';
+	import unit from '../common/unit.js';
 	export default {
 		filters: {
 			...unit
@@ -47,7 +48,7 @@
 		computed: {
 			...mapState({
 				playStatus: ({
-					audio,
+					audio
 				}) => audio.playStatus,
 				durationTime: ({
 					audio
@@ -70,17 +71,18 @@
 				'PlayOrPause',
 				'PreOrNext',
 				'sliderToPlay'
-			])
-		},
-		data() {
-			return {
+			]),
+			toDetailPage() {
+				uni.navigateTo({
+					url: '/pages/musicDetail/musicDetail'
+				});
 			}
 		},
 		mounted() {
 			this.init()
 		},
 		destroyed() {
-			ths.description()
+			this.destruction()
 		}
 	}
 </script>
